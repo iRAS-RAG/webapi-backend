3908bc660a748b54e4f76d77c7af27e06b12f6ec
P4nzerr <enderthelord@gmail.com>
2026-02-03 18:11:49 +0700

    refactor: rename and reorganize auth related infra services

---
+++ IRasRag.Application/Common/Interfaces/Auth/IHashingService.cs
@@ -0,0 +1,8 @@
+﻿namespace IRasRag.Application.Common.Interfaces.Auth
+{
+    public interface IHashingService
+    {
+        string Hash(string value);
+        bool VerifyHash(string value, string hashedValue);
+    }
+}
--- IRasRag.Application/Common/Interfaces/Auth/IPasswordHasher.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace IRasRag.Application.Common.Interfaces.Auth
-{
-    public interface IPasswordHasher
-    {
-        string HashPassword(string password);
-        bool VerifyPassword(string password, string hashedPassword);
-    }
-}
