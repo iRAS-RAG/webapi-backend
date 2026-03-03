842edd479599f3db3013067ef5e4bf348daf1bf6
P4nzerr <enderthelord@gmail.com>
2026-03-03 22:18:17 +0700

    feat(application): add interfaces for file validation, extraction, and cloud storage

---
+++ IRasRag.Application/Common/Interfaces/FileExtractor/IFileTextExtractorResolver.cs
@@ -0,0 +1,7 @@
+﻿namespace IRasRag.Application.Common.Interfaces.FileExtractor
+{
+    public interface IFileTextExtractorResolver
+    {
+        string ExtractText(Stream fileStream, string fileName);
+    }
+}
