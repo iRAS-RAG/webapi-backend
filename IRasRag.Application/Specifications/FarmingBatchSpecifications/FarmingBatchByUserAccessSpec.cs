4221a0e8df9ebb9bc01434f9a4865dae7b3c09e8
P4nzerr <enderthelord@gmail.com>
2026-03-20 19:58:54 +0700

    feat(application): add user-access specification for farming batches
    - Create FarmingBatchByUserAccessSpec to enforce user permission checks
    - Queries farming batches accessible to a specific user via UserFarms relationship
    - Used by CreateFeedingLogAsync to validate batch ownership before recording feed events

---
+++ IRasRag.Application/Specifications/FarmingBatchSpecifications/FarmingBatchByUserAccessSpec.cs
@@ -0,0 +1,15 @@
+﻿using Ardalis.Specification;
+using IRasRag.Domain.Entities;
+
+namespace IRasRag.Application.Specifications.FarmingBatchSpecifications
+{
+    public class FarmingBatchByUserAccessSpec : Specification<FarmingBatch>
+    {
+        public FarmingBatchByUserAccessSpec(Guid batchId, Guid userId)
+        {
+            Query
+                .AsNoTracking()
+                .Where(fb => fb.Id == batchId && fb.FishTank.Farm.UserFarms.Any(uf => uf.UserId == userId));
+        }
+    }
+}
